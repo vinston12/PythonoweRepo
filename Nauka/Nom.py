81046ac2cbb73d5211f893edc598c107e6d12892
vinston12 <dom.boberek@gmail.com>
2018-10-04 21:54:13 +0200

    Skonczony pierwszy kurs python

--- Nauka/Nom.py
+++ Nauka/Nom.py
@@ -1,2 +1,37 @@
+# Pamiętaj komentarze to '#' a nie '//' !!
 my_name = "Dominik"
-print("Siema jestem" + my_name)
+
+print("Siema, jestem " + my_name)
+
+
+# w pythonie można używać ' ' zamiast " " (single quotes, double quotes)
+print('Hej jestem ' + my_name)
+
+# Pamietaj ze kilka razy mozna przypisywac do zmiennej rozne wartosci!
+
+# teraz w zmiennej my_name siedzi wartosc typu String "Sylwia"
+my_name = "Sylwia"
+
+print("Imie mojej kolezanki to: " + my_name)
+
+# w pythonie możemy wypisać duży tekst używając trzech  " zamiast jednego
+
+to_you = """
+    Stranger, if you passing meet me and desire to speak to me, 
+    why should you not speak to me? 
+    And why should I not speak to you?
+    """
+
+print(to_you)
+
+# ostateczny program
+
+my_age = 18
+
+half_my_age  = my_age/2
+
+greeting = "Hello "
+
+name = "Dominik"
+
+greeting_with_name = greeting + name
